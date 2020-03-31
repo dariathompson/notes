e91da388105b77ca6d0241a009665ce8d7d7e124
Rafael <r.hgar78@gmail.com>
2020-03-31 18:38:54 +0200

    note list shows now 20 characters2

--- spec/note-list-view-tests.js
+++ spec/note-list-view-tests.js
@@ -1,9 +1,9 @@
 function testViewNotes() {
   var note_list = new NoteList;
-  note_list.addNote('This is a note');
+  note_list.addNote('12345678912345678911$$$$');
   note_list.addNote('This is another note');
   var note_list_view = new NoteListView(note_list);
-  assert.isTrue(note_list_view.viewNotes() === "<ul><li><div>This is a note</div></li><li><div>This is another note</div></li></ul>");
+  assert.isTrue(note_list_view.viewNotes() === "<ul><li><div>12345678912345678911</div></li><li><div>This is another note</div></li></ul>");
 };
 
 testViewNotes()
