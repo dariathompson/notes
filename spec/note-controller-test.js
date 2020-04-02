17b92ddf33377c21d19c79411195cccd71bcb4e2
dariathompson <daria.kambalina@gmail.com>
2020-04-02 15:31:35 +0100

    submitting form creates new note

--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -1,23 +1,14 @@
 // function viewNotesInHtml() {
-//   class NoteDouble {
-//     constructor(text){
-//       this.text = text;
+//   function NotelistviewDouble() {
+//   }
+//   NotelistviewDouble.prototype = {
+//       showList: function(){
+//         return "<ul><li><div>test</div></li><li><div>test2</div></li></ul>"
+//       }
 //     }
-//     read() {
-//       return this.text;
-//     }
-//   };
-//   class NoteListDouble {
-//     constructor() {
-//       this.notes = [];
-//     }
-//     addNote(text) {
-//       this.notes.push(new NoteDouble(text));
-//     }
-//   //   displayNotes() {
-//   //     return this.notes;
-//   // }
-//   };
+//     let notecontroller = new Notecontroller(new NotelistviewDouble());
+//     notecontroller.noteListView = new NotelistviewDouble();
+//     notecontroller.outputToHTML()
 
 //   var list = new NoteListDouble;
 //   list.addNote('Rafa is amazing');
